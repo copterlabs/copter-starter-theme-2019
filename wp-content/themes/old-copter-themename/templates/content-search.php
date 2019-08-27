b4a9f4623a4f433f74cf9c32571d5b7e50bfe897
Alex Newman <thedotmack@gmail.com>
2019-08-27 12:54:42 -0400

    Removed old starter theme

--- wp-content/themes/old-copter-themename/templates/content-search.php
+++
@@ -1,9 +0,0 @@
-<article <?php post_class('copter--post'); ?>>
-  <header>
-    <h2 class="copter--post__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php if (get_post_type() === 'post') { get_template_part('templates/entry-meta'); } ?>
-  </header>
-  <div class="copter--post__excerpt">
-    <?php the_excerpt(); ?>
-  </div>
-</article>
